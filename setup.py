13e449617de0f301ae1c37755a837c6d8efcb86f
Dmytri Kleiner <dk@telekommunisten.net>
2009-05-25 23:45:38 +0200

    added testfixtures and setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(name='telekommie',
+    version='0.1',
+    description='Telekommunisten Integration Environment',
+    author='Dmytri Kleiner',
+    author_email='dk@telekommunisten.net',
+    url='http://cgit.telekommunisten.org/telekommie',
+    packages=['telekommie'],
+)
+
