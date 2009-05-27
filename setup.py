36be7191f7b77ba5c254f860adc6d14c91128b9e
Dmytri Kleiner <dk@telekommunisten.net>
2009-05-27 23:23:24 +0200

    clean up, test coverage, etc

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
-from distutils.core import setup
+#from distutils.core import setup
+from setuptools import setup
 setup(name='telekommie',
     version='0.1',
     description='Telekommunisten Integration Environment',
     author='Dmytri Kleiner',
     author_email='dk@telekommunisten.net',
     url='http://cgit.telekommunisten.org/telekommie',
+    test_suite='nose.collector',
     packages=['telekommie'],
 )
 
